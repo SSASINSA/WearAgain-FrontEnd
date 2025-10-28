a1013120b5e1b3305d3ec6260b9d92711a49ee0d
jenny1zzang <cyberkhl@naver.com>
2025-10-28 11:19:16 +0900

    #44 행사 상세 페이지 nav 추가

--- src/app/navigation/MainNavigation.tsx
+++ src/app/navigation/MainNavigation.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import BottomTabNavigation from './BottomTabNavigation';
 import PostDetailScreen from '../../screens/community/PostDetailScreen';
+import EventDetailScreen from '../../screens/event/EventDetailScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +11,7 @@ export default function MainNavigation() {
     <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name="Main" component={BottomTabNavigation} />
       <Stack.Screen name="PostDetail" component={PostDetailScreen} />
+      <Stack.Screen name="EventDetail" component={EventDetailScreen}/>
     </Stack.Navigator>
   );
 }
